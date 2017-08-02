4119df58fa4d9bc596b358185808de11a349028b
RunasSudo <runassudo@yingtongli.me>
2017-08-02 23:12:54 +1000

    Start multi-tab view

--- anatview/qtui.py
+++ anatview/qtui.py
@@ -7,7 +7,7 @@ from . import renderer
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
-from PyQt5.QtWidgets import QApplication, QGridLayout, QHeaderView, QLabel, QLineEdit, QMessageBox, QPushButton, QTreeView, QWidget
+from PyQt5.QtWidgets import QApplication, QGridLayout, QHeaderView, QLabel, QLineEdit, QMessageBox, QPushButton, QTabWidget, QTreeView, QWidget
 
 import sys
 
@@ -30,6 +30,35 @@ class MainUI(QWidget):
 		self.search_box.returnPressed.connect(self.search_box_return)
 		self.grid.addWidget(QLabel('Search:'), 0, 0)
 		self.grid.addWidget(self.search_box, 0, 1, 1, 15)
+		
+		self.list_tab = ListTab(self)
+		
+		self.tab_widget = QTabWidget()
+		self.tab_widget.addTab(self.list_tab, 'List')
+		
+		self.grid.addWidget(self.tab_widget, 1, 0, 7, 16)
+		
+		# Render button
+		self.render_button = QPushButton('Render')
+		self.render_button.clicked.connect(self.render_button_click)
+		self.grid.addWidget(self.render_button, 8, 15)
+		
+		self.search_box.setFocus(True)
+	
+	def search_box_return(self):
+		self.list_tab.search_box_return()
+	
+	def render_button_click(self):
+		self.list_tab.render_button_click()
+
+class ListTab(QWidget):
+	def __init__(self, main_ui):
+		super().__init__()
+		
+		self.main_ui = main_ui
+		
+		self.grid = QGridLayout()
+		self.setLayout(self.grid)
 		
 		self.tree_model = QStandardItemModel()
 		self.tree_model.setHorizontalHeaderItem(0, QStandardItem('ID'))
@@ -66,13 +95,6 @@ class MainUI(QWidget):
 		self.tree_view.header().resizeSection(2, 32)
 		self.tree_view.header().setStretchLastSection(False)
 		self.grid.addWidget(self.tree_view, 1, 0, 7, 16)
-		
-		# Render button
-		self.render_button = QPushButton('Render')
-		self.render_button.clicked.connect(self.render_button_click)
-		self.grid.addWidget(self.render_button, 8, 15)
-		
-		self.search_box.setFocus(True)
 	
 	def search_box_return(self):
 		# not_before is loc
@@ -82,7 +104,7 @@ class MainUI(QWidget):
 				nonlocal after_before # ily python 3
 				
 				# Check for match
-				if after_before and self.search_box.text() in item.name:
+				if after_before and self.main_ui.search_box.text() in item.name:
 					return loc
 				
 				# Are we passing the current selection?
@@ -132,5 +154,5 @@ class MainUI(QWidget):
 					add_with_children(loc, v)
 		print()
 		
-		self.renderer.set_locs(locs)
-		self.renderer.render()
+		self.main_ui.renderer.set_locs(locs)
+		self.main_ui.renderer.render()
